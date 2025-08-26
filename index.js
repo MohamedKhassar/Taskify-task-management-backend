5ecbff52412d00eb8d72dd17fb0ee44c9d5e67d0
Mohamed Khassar <mohamedkhassar775@gmail.com>
2025-08-26 15:05:38 +0100

    Refactor: Update server configuration to use environment variables for PORT and HOST

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ import { config } from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import { connectDB } from "./connectDB.js";
 const FRONT_ORIGIN = process.env.FRONT_ORIGIN || "http://localhost:5173";
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
 
 connectDB();
 config();
 
-app.use(express.json({
-  strict: true,
-  verify: (req, res, buf, encoding) => {
-    try {
-      JSON.parse(buf);
-    } catch (e) {
-      throw new SyntaxError("Invalid JSON");
-    }
-  }
-}));
+app.use(
+  express.json({
+    strict: true,
+    verify: (req, res, buf, encoding) => {
+      try {
+        JSON.parse(buf);
+      } catch (e) {
+        throw new SyntaxError("Invalid JSON");
+      }
+    },
+  })
+);
 
 // Global error handler
 app.use((err, req, res, next) => {
@@ -28,9 +32,9 @@ app.use((err, req, res, next) => {
   next(err);
 });
 app.use(express.urlencoded({ extended: true })); // for form data
-app.use(cors({ origin: FRONT_ORIGIN }));
+app.use(cors({ origin: [FRONT_ORIGIN,"http://192.168.1.3:5173"] }));
 app.use("/api/auth", authRoutes);
 
-app.listen(8080, () => {
+app.listen(PORT, HOST, () => {
   console.log("Server is running on port 8080");
 });
